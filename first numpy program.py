ab2d66d4d9f2a19842ac2e9901622c7fa54baddf
Rupjung <rpjmalla@gmail.com>
2024-01-09 21:32:32 +0545

    Learning Basics of numpy

---
+++ first numpy program.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array1 = np.array([1, 2, 3])
+
+print(array1)
+
+
+#the np.array() method creates an array from the numbers list and assigns it to the array1 variable. 
+#The output is a NumPy array.
---
+++ numpy from scratch.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array1 = np.zeros(5)
+
+print(array1)
+
+array2 = np.ones(3)
+print(array2)
+
+array3 = np.arange(1, 15, 3) #works like range()
+print(array3)
